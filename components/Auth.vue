e75513a60f28fc94fe904493956102316eaa2123
Maxime Coriton <maxime.coriton@gmail.com>
2023-01-02 15:08:37 +0100

    test cookie sol 1

--- components/Auth.vue
+++ components/Auth.vue
@@ -1,38 +1,52 @@
 <template>
-    <form class="row flex-center flex" @submit.prevent="handleLogin">
-      <div class="col-6 form-widget">
-        <h1 class="header">Supabase + Nuxt 3</h1>
-        <p class="description">Sign in via magic link with your email below</p>
-        <div>
-          <input class="inputField" type="email" placeholder="Your email" v-model="email" />
-        </div>
-        <div>
-          <input
-            type="submit"
-            class="button block"
-            :value="loading ? 'Loading' : 'Send magic link'"
-            :disabled="loading"
-          />
-        </div>
+  <form class="row flex-center flex" @submit.prevent="handleLogin">
+    <div class="col-6 form-widget">
+      <h1 class="header">Supabase + Nuxt 3</h1>
+      <p class="description">Sign in via magic link with your email below</p>
+      <div>
+        <input
+          class="inputField"
+          type="email"
+          placeholder="Your email"
+          v-model="email"
+        />
       </div>
-    </form>
-  </template>
-  
-  <script setup>
-    const supabase = useSupabaseClient()
-  
-    const loading = ref(false)
-    const email = ref('')
-    const handleLogin = async () => {
-      try {
-        loading.value = true
-        const { error } = await supabase.auth.signInWithOtp({ email: email.value })
-        if (error) throw error
-        alert('Check your email for the login link!')
-      } catch (error) {
-        alert(error.error_description || error.message)
-      } finally {
-        loading.value = false
-      }
-    }
-  </script>
+      <div>
+        <input
+          type="submit"
+          class="button block"
+          :value="loading ? 'Loading' : 'Send magic link'"
+          :disabled="loading"
+        />
+      </div>
+    </div>
+  </form>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient();
+
+const loading = ref(false);
+const email = ref("");
+const handleLogin = async () => {
+  if(!!response){
+      const accessToken = useCookie('sb-access-token')
+      const refreshToken = useCookie('sb-refresh-token')
+      accessToken.value = response.session?.access_token ?? null
+      refreshToken.value = response.session?.refresh_token ?? null
+  }
+  if(!!error){
+      errorMsg.value = error.message
+  }
+  try {
+    loading.value = true;
+    const { error } = await supabase.auth.signInWithOtp({ email: email.value });
+    if (error) throw error;
+    alert("Check your email for the login link!");
+  } catch (error) {
+    alert(error.error_description || error.message);
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
